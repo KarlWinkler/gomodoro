d8fc2face1a618f3934cb7230b3420c71a1e883b
karlWinkler <karlw200@gmail.com>
2025-02-21 00:59:15 -0700

    add alarm using notify-send when timer is up

--- src/main.go
+++ src/main.go
@@ -4,10 +4,13 @@ import (
   "bufio"
   "fmt"
   "os"
+  "os/exec"
   "strings"
   "time"
   "strconv"
 )
+
+type Alarm func(string)
 
 func setup(reader *bufio.Reader) (string, string) {
   fmt.Println("work time (minutes): ")
@@ -20,7 +23,7 @@ func setup(reader *bufio.Reader) (string, string) {
   return workTime, breakTime
 }
 
-func run(wt, bt string) {
+func run(wt, bt string, alarm Alarm) {
   wtInt, wtErr := strconv.Atoi(wt)
   btInt, btErr := strconv.Atoi(bt)
 
@@ -29,30 +32,36 @@ func run(wt, bt string) {
     return
   }
 
-  workTimer := wtInt * 60
-  breakTimer := btInt * 60
   for true {
+    workTimer := wtInt * 60
+    breakTimer := btInt * 60
+
     for workTimer > 0 {
       timer := time.NewTimer(1 * time.Second)
 
       <- timer.C
-      fmt.Printf("\r%02d:%02d", int(workTimer / 60), workTimer % 60)
+      fmt.Printf("\rWork: %02d:%02d", int(workTimer / 60), workTimer % 60)
       workTimer--
     }
+    alarm("Break Time!")
     for breakTimer > 0 {
       timer := time.NewTimer(1 * time.Second)
 
       <- timer.C
-      fmt.Printf("\r%02d:%02d", int(breakTimer / 60), breakTimer % 60)
+      fmt.Printf("\rBreak: %02d:%02d", int(breakTimer / 60), breakTimer % 60)
       breakTimer--
     }
-    break
+    alarm("Work Time!")
   }
-  fmt.Println(workTimer, breakTimer)
+}
+
+func notifySend(message string) {
+  cmd := exec.Command("notify-send", message)
+  _ = cmd.Run()
 }
 
 func main() {
   reader := bufio.NewReader(os.Stdin)
   wt, bt := setup(reader)
-  run(wt, bt)
+  run(wt, bt, notifySend)
 }
