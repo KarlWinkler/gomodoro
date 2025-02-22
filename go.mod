ad022c2b6bc234fd00b9edb9d451d15227e18dfd
karlWinkler <karlw200@gmail.com>
2025-02-22 11:55:22 -0700

    add rudimentary pause functionality

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/karlwinkler/gomodoro
 
 go 1.24.0
+
+require (
+	github.com/faiface/beep v1.1.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/term v0.29.0 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -13,17 +13,17 @@ import (
 type Alarm func(string)
 
 func setup(reader *bufio.Reader) (string, string) {
-  fmt.Println("work time (minutes): ")
+  fmt.Print("work time (minutes): ")
   wtIn, _ := reader.ReadString('\n')
   workTime := strings.Replace(wtIn, "\n", "", -1)
-  fmt.Println("break time (minutes): ")
+  fmt.Print("break time (minutes): ")
   btIn, _ := reader.ReadString('\n')
   breakTime := strings.Replace(btIn, "\n", "", -1)
 
   return workTime, breakTime
 }
 
-func run(wt, bt string, alarm Alarm) {
+func run(wait, resume chan bool, wt, bt string, alarm Alarm) {
   wtInt, wtErr := strconv.Atoi(wt)
   btInt, btErr := strconv.Atoi(bt)
 
@@ -37,11 +37,18 @@ func run(wt, bt string, alarm Alarm) {
     breakTimer := btInt * 60
 
     for workTimer > 0 {
-      timer := time.NewTimer(1 * time.Second)
 
-      <- timer.C
-      workTimer--
-      fmt.Printf("\rWork: %02d:%02d", int(workTimer / 60), workTimer % 60)
+      select {
+        case <-wait:
+          <-resume
+        default:
+          timer := time.NewTimer(1 * time.Second)
+
+          <- timer.C
+          workTimer--
+          fmt.Printf("\rWork: %02d:%02d", int(workTimer / 60), workTimer % 60)
+      }
+
     }
     alarm("Break Time!")
     for breakTimer > 0 {
@@ -55,6 +62,33 @@ func run(wt, bt string, alarm Alarm) {
   }
 }
 
+func manage(wait, resume chan bool, reader *bufio.Reader) {
+  w := false
+  fp, _ := os.Create("tmp")
+  defer fp.Close()
+  writer := bufio.NewWriter(fp)
+  _, err := fmt.Fprintf(writer, "in\n")
+  if err != nil {
+    panic(err)
+  }
+  for true {
+    in, _ := reader.ReadString('\n')
+    fmt.Fprintf(writer, ":%b\n", in)
+    in = strings.Replace(in, "\n", "", -1)
+    if in == " " {
+      fmt.Println(w)
+      w = !w
+      if w {
+        wait <- true
+      } else {
+        resume <- true
+      }
+    }
+    writer.Flush()
+    fp.Sync()
+  }
+}
+
 func notifySend(message string) {
   cmd := exec.Command("notify-send", message)
   _ = cmd.Run()
@@ -63,5 +97,10 @@ func notifySend(message string) {
 func main() {
   reader := bufio.NewReader(os.Stdin)
   wt, bt := setup(reader)
-  run(wt, bt, notifySend)
+
+  wait := make(chan bool)
+  resume := make(chan bool)
+
+  go run(wait, resume, wt, bt, notifySend)
+  manage(wait, resume, reader)
 }
