35b5f0649dd339d9a55577cc385ab5d96099b4cd
karlWinkler <karlw200@gmail.com>
2025-02-25 10:29:33 -0700

    continue refactor

--- src/setup.go
+++ src/setup.go
@@ -3,16 +3,39 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"strconv"
 	"strings"
+
 )
 
-func setup(reader *bufio.Reader) (string, string) {
-  fmt.Print("work time (minutes): ")
-  wtIn, _ := reader.ReadString('\n')
-  workTime := strings.Replace(wtIn, "\n", "", -1)
-  fmt.Print("break time (minutes): ")
-  btIn, _ := reader.ReadString('\n')
-  breakTime := strings.Replace(btIn, "\n", "", -1)
+func setup(reader *bufio.Reader) (int, int) {
+	wt := _get_time_with_retry("work time: ", reader)
+	bt := _get_time_with_retry("break time: ", reader)
 
-  return workTime, breakTime
+  return wt, bt
+}
+
+// work time (minutes): 
+
+func _get_time_with_retry(prompt string, reader *bufio.Reader) int {
+	for true {
+		time, err := _get_time(prompt, reader)
+
+		if err == nil {
+			return time
+		} else {
+			fmt.Println("Not a number! Please try again,")
+		}
+	}
+	panic("Escaped Loop in { setup.go::_get_time_with_retry }")
+}
+
+func _get_time(prompt string, reader *bufio.Reader) (int, error) {
+	fmt.Print(prompt)
+	in, _ := reader.ReadString('\n')
+	in = strings.Replace(in, "\n", "", -1)
+
+	time, err := strconv.Atoi(in)
+
+	return time, err
 }
--- src/timer.go
+++ src/timer.go
@@ -2,55 +2,38 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 )
 
 type Alarm func(string)
 
-func run(wait, resume chan bool, wt, bt string, alarm Alarm) {
-  wtInt, wtErr := strconv.Atoi(wt)
-  btInt, btErr := strconv.Atoi(bt)
-
-  if wtErr != nil || btErr != nil {
-    fmt.Println("one of your inputs was invalid")
-    return
-  }
-
+func run(wait, resume chan bool, wt, bt int, alarm Alarm) {
   for true {
-    workTimer := wtInt * 60
-    breakTimer := btInt * 60
+		_countdown(wait, resume, "Work", wt * 60)
+		alarm("Break Time!")
 
-    fmt.Printf("\033[2K\rWork: %02d:%02d", int(workTimer / 60), workTimer % 60)
-    for workTimer > 0 {
-      timer := time.NewTimer(1 * time.Second)
-
-      select {
-        case <- wait:
-          fmt.Printf("\033[2K\rWork: %02d:%02d ||", int(workTimer / 60), workTimer % 60)
-          <- resume
-          fmt.Printf("\033[2K\rWork: %02d:%02d", int(workTimer / 60), workTimer % 60)
-        case <- timer.C:
-          workTimer--
-          fmt.Printf("\033[2K\rWork: %02d:%02d", int(workTimer / 60), workTimer % 60)
-      }
-    }
-    alarm("Break Time!")
-
-    fmt.Printf("\033[2K\rBreak: %02d:%02d", int(breakTimer / 60), breakTimer % 60)
-    for breakTimer > 0 {
-      timer := time.NewTimer(1 * time.Second)
-
-      select {
-        case <- wait:
-          fmt.Printf("\033[2K\rBreak: %02d:%02d ||", int(breakTimer / 60), breakTimer % 60)
-          <- resume
-          fmt.Printf("\033[2K\rBreak: %02d:%02d", int(breakTimer / 60), breakTimer % 60)
-        case <- timer.C:
-          breakTimer--
-          fmt.Printf("\033[2K\rBreak: %02d:%02d", int(breakTimer / 60), breakTimer % 60)
-      }
-    }
-    alarm("Work Time!")
+		_countdown(wait, resume, "Break", bt * 60)
+		alarm("Break Time!")
   }
+}
+
+func _countdown(wait, resume chan bool, mode string, currentTime int) {
+	_printTimer(mode, currentTime)
+	for currentTime > 0 {
+		timer := time.NewTimer(1 * time.Second)
+
+		select {
+			case <- wait:
+				_printTimer(mode, currentTime)
+				<- resume
+				_printTimer(mode, currentTime)
+			case <- timer.C:
+				currentTime--
+				_printTimer(mode, currentTime)
+		}
+	}
+}
+
+func _printTimer(mode string, timer int) {
+	fmt.Printf("\033[2K\r%s: %02d:%02d", mode, int(timer / 60), timer % 60)
 }
